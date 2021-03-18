e1648ec1044161db33fcd4897b106f3919e96a82
phelang.qhu@hotmail.com <phelang.qhu@gmail.com>
2021-03-18 15:35:32 +0200

    Initial TrackServer Commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+require('./models/User')
+require('./models/Track')
+require('./models/Signup')
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const authRoutes = require('./routes/authRoutes')
+const trackRoutes = require('./routes/trackRoutes')
+const requireAuth = require('./middlewares/requireAuth')
+const authAccountSettings = require('./routes/accountSetting')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(authRoutes)
+app.use(trackRoutes)
+app.use(authAccountSettings)
+
+const mongoUri =
+  'mongodb+srv://root:AY8tDEoA1ic9BHWA@track.it6jr.mongodb.net/track?retryWrites=true&w=majority'
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+})
+
+mongoose.connection.on('connected', () => {
+  console.log('Connection to mongo instance')
+})
+
+mongoose.connection.on('error', (error) => {
+  console.log('Error connecting to mongo')
+})
+
+app.get('/', requireAuth, (req, res) => {
+  res.send(`Your email : ${req.user.email}`)
+})
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
+})

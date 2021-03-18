3484a976f7e0435e42c3fd13b9b4a8b6eebd01f7
phelang.qhu@hotmail.com <phelang.qhu@gmail.com>
2021-03-18 17:21:24 +0200

    Solve app.port

--- src/index.js
+++ src/index.js
@@ -36,6 +36,10 @@ app.get('/', requireAuth, (req, res) => {
   res.send(`Your email : ${req.user.email}`)
 })
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000')
+app.listen(process.env.PORT || 3000, () => {
+  console.log(
+    'Express server listening on port %d in %s mode',
+    this.address().port,
+    app.settings.env
+  )
 })
